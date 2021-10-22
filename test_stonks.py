5cf85b2a342739be39c501baa2cdfe8855342b70
Vineet Tiruvadi <virati@gmail.com>
2021-10-21 23:10:10 -0700

    small update

--- test_stonks.py
+++ test_stonks.py
@@ -7,11 +7,14 @@ import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-comp1 = yf.download(tickers='FB', period='5d',interval='5m')
-comp2 = yf.download(tickers='AMZN', period='5d', interval='5m')
-comp3 = yf.download(tickers='TSLA',period='5d',interval='5m')
+comp1 = yf.download(tickers='FB', period='10',interval='15m')
+comp2 = yf.download(tickers='AMZN', period='10d', interval='15m')
+comp3 = yf.download(tickers='TSLA',period='10d',interval='15m')
+
+healthcare = yf.download(tickers='VHT', period='10d', interval='15m')
 
 fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
-ax.scatter(comp1,comp2,comp3)
+#ax.scatter(comp1,comp2,comp3)
+ax.scatter(healthcare, comp1, comp2, projection='3d')
 plt.show()
