982b81a1777b5176dce327b758ddd159be30cb7a
Vineet Tiruvadi <virati@gmail.com>
2022-08-23 01:40:56 +0000

    added sector labels

--- notebooks/basic_phase.py
+++ notebooks/basic_phase.py
@@ -1,5 +1,7 @@
 # %%
 import yfinance as yf
+from scipy.stats import zscore
+import pandas as pd
 
 nasdq = yf.Ticker("NDX").history(period="1y", interval="1d", actions=False)
 sp = yf.Ticker("^GSPC").history(period="1y", interval="1d", action=False)
@@ -11,3 +13,38 @@ plt.plot(nasdq["Close"])
 plt.plot(sp["Close"])
 
 plt.show()
+
+
+#%%
+
+nasdq_sig = zscore(nasdq["Close"])
+sp_sig = zscore(sp["Close"])[:-1]
+
+plt.plot(nasdq_sig, sp_sig)
+
+
+#%%
+sectors = {
+    "IAK",
+    "FCG",
+    "XME",
+    "XAR",
+    "PEJ",
+    "FTXG",
+    "SMH",
+    "PPH",
+    "REZ",
+    "EWCO",
+    "JHMU",
+}
+timeseries = {key: [] for key in sectors}
+
+for key in sectors:
+    timeseries[key] = yf.Ticker(key).history(period="1y", interval="1d", actions=False)[
+        "Close"
+    ]
+
+#%%
+import numpy as np
+
+design_matrix = np.array([timeseries[key] for key in sectors])
